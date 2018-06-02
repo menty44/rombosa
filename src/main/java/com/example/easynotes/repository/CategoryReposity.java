65ebc0fefb59d00edb93ba3a44df366674bc64ef
Fredrick Oluoch <menty44@gmail.com>
2018-06-02 21:52:01 +0300

    added category repository

---
+++ src/main/java/com/example/easynotes/repository/CategoryReposity.java
@@ -0,0 +1,14 @@
+package com.example.easynotes.repository;/**
+ * Created by admin on 6/2/18.
+ */
+
+/**
+ * Fredrick Oluoch
+ * http://www.blaqueyard.com
+ * 0720106420 | 0722508906
+ * email: menty44@gmail.com
+ */
+
+
+public interface CategoryReposity {
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by admin on 5/30/18.
+ * Fredrick Oluoch
+ * http://www.blaqueyard.com
+ * 0720106420 | 0722508906
+ * email: menty44@gmail.com
+ */
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    // document.getElementById("flipkart-navbar").style.width = "50%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
