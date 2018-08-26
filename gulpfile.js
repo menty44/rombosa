0c20e86db70d385b2f6fa75fcca9509d434e4b60
Fredrick Oluoch <menty44@gmail.com>
2018-08-26 11:25:32 +0300

    gulp bug

--- gulpfile.js
+++ gulpfile.js
@@ -16,31 +16,31 @@ gulp.task('default', function() {
         .pipe(gulp.dest('src/main/resources/static/prod/'));
 });
 
-//gulp.task('default');
-// gulp.task('default', function (cb) {
-//   pump([
-//         gulp.src('src/main/resources/static/js/*.js'),
-//         uglify(),
-//         gulp.dest('src/main/resources/static/prod/')
-//     ],
-//     cb
-//   );
-// });
+gulp.task('default');
+gulp.task('default', function (cb) {
+  pump([
+        gulp.src('src/main/resources/static/js/*.js'),
+        uglify(),
+        gulp.dest('src/main/resources/static/prod/')
+    ],
+    cb
+  );
+});
 
-// gulp.task('default', function() {
-//     gulp.src('src/main/resources/static/js/*.js')
-//         .pipe(uglify('app.js', {
-//             mangle: false,
-//             output: {
-//                 beautify: true
-//             }
-//         }))
-//         .pipe(gulp.dest('src/main/resources/static/prod/'))
-// });
+gulp.task('default', function() {
+    gulp.src('src/main/resources/static/js/*.js')
+        .pipe(uglify('app.js', {
+            mangle: false,
+            output: {
+                beautify: true
+            }
+        }))
+        .pipe(gulp.dest('src/main/resources/static/prod/'))
+});
 
-// gulp.task('default', function(){
-//     gulp.src('src/main/resources/static/js/*.js')
-//         .pipe(concat(path.MINIFIED_OUT))
-//         .pipe(uglify())
-//         .pipe('src/main/resources/static/prod/');
-// });
+gulp.task('default', function(){
+    gulp.src('src/main/resources/static/js/*.js')
+        .pipe(concat(path.MINIFIED_OUT))
+        .pipe(uglify())
+        .pipe('src/main/resources/static/prod/');
+});
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -6,91 +6,92 @@
  * email: menty44@gmail.com
  */
 
-var routerApp = angular.module("routerApp");
+// var routerApp = angular.module("routerApp");
 
-routerApp.controller('studentController', function($scope) {
-    $scope.student = {
-        firstName: "Mahesh",
-        lastName: "Parashar",
-        fees:500,
+// routerApp.controller('studentController', function($scope) {
+//     console.log('studentController');
+//     $scope.student = {
+//         firstName: "Mahesh",
+//         lastName: "Parashar",
+//         fees:500,
+//
+//         subjects:[
+//             {name:'Physics',marks:70},
+//             {name:'Chemistry',marks:80},
+//             {name:'Math',marks:65}
+//         ],
+//
+//         fullName: function() {
+//             var studentObject;
+//             studentObject = $scope.student;
+//             return studentObject.firstName + " " + studentObject.lastName;
+//         }
+//     };
+// });
 
-        subjects:[
-            {name:'Physics',marks:70},
-            {name:'Chemistry',marks:80},
-            {name:'Math',marks:65}
-        ],
+// routerApp.controller('registerController', function($scope, $http, $location, $cookieStore, $rootScope) {
+//
+//
+//     $scope.test = 'tetetette';
+//     $scope.master = function master() {
+//
+//         console.log('test register');
+//         $scope.test = {
+//             "firstname": "",
+//             "lastname": "",
+//             "mobile": "",
+//             "email": "",
+//             "password": "",
+//             "activated": "",
+//             "createdAt": "",
+//             "updatedAt": ""
+//         };
+//
+//         var data = {
+//             "UserID": $scope.user.id,
+//             "JackpotID": $scope.fred.JackpotID,
+//             "Amount": $scope.betStake,
+//             "games": betslip
+//         };
+//
+//         console.log("THIS IS THE JACKPOTID POSTED TO CITYBET SERVER");
+//         console.log($scope.fred.JackpotID);
+//         console.log("THIS IS THE DATA POSTED TO CITYBET SERVER");
+//         console.log(data);
+//         var url = 'https://api.citybet.co.ke/v1/jackpot/bet';
+//
+//         $http.post(url, data).then(function (response) {
+//             console.log("This was the response", response);
+//             //$scope.clearBet();
+//             $rootScope.$broadcast("globalDialogs.addDialog", {
+//                 type: 'success',
+//                 title: 'Success',
+//                 content: 'Your Bet was submitted Successfully. '+ 'Your BetID is ' + response.data.BetID
+//             });
+//             $scope.removeall();
+//         })
+//
+//
+//
+// }
+// });
 
-        fullName: function() {
-            var studentObject;
-            studentObject = $scope.student;
-            return studentObject.firstName + " " + studentObject.lastName;
-        }
-    };
-});
-
-routerApp.controller('registerController', function($scope, $http, $location, $cookieStore, $rootScope) {
-
-
-    $scope.test = 'tetetette';
-    $scope.master = function master() {
-
-        console.log('test register');
-        $scope.test = {
-            "firstname": "",
-            "lastname": "",
-            "mobile": "",
-            "email": "",
-            "password": "",
-            "activated": "",
-            "createdAt": "",
-            "updatedAt": ""
-        };
-
-        var data = {
-            "UserID": $scope.user.id,
-            "JackpotID": $scope.fred.JackpotID,
-            "Amount": $scope.betStake,
-            "games": betslip
-        };
-
-        console.log("THIS IS THE JACKPOTID POSTED TO CITYBET SERVER");
-        console.log($scope.fred.JackpotID);
-        console.log("THIS IS THE DATA POSTED TO CITYBET SERVER");
-        console.log(data);
-        var url = 'https://api.citybet.co.ke/v1/jackpot/bet';
-
-        $http.post(url, data).then(function (response) {
-            console.log("This was the response", response);
-            //$scope.clearBet();
-            $rootScope.$broadcast("globalDialogs.addDialog", {
-                type: 'success',
-                title: 'Success',
-                content: 'Your Bet was submitted Successfully. '+ 'Your BetID is ' + response.data.BetID
-            });
-            $scope.removeall();
-        })
-
-
-
-}
-});
-
-routerApp.controller('loginController', function($scope, $http, $location, $cookieStore, $rootScope) {
-    $scope.student = {
-        firstName: "Mahesh",
-        lastName: "Parashar",
-        fees:500,
-
-        subjects:[
-            {name:'Physics',marks:70},
-            {name:'Chemistry',marks:80},
-            {name:'Math',marks:65}
-        ],
-
-        fullName: function() {
-            var studentObject;
-            studentObject = $scope.student;
-            return studentObject.firstName + " " + studentObject.lastName;
-        }
-    };
-});
+// routerApp.controller('loginController', function($scope, $http, $location, $cookieStore, $rootScope) {
+//     $scope.student = {
+//         firstName: "Mahesh",
+//         lastName: "Parashar",
+//         fees:500,
+//
+//         subjects:[
+//             {name:'Physics',marks:70},
+//             {name:'Chemistry',marks:80},
+//             {name:'Math',marks:65}
+//         ],
+//
+//         fullName: function() {
+//             var studentObject;
+//             studentObject = $scope.student;
+//             return studentObject.firstName + " " + studentObject.lastName;
+//         }
+//     };
+// });
